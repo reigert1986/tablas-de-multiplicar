983631818721a4b6afea53acc97d1bd30d03e632
reigert1986 <reigert_lorigador@outlook.com>
2021-08-05 20:31:22 -0300

    "dar las tabals de multiplicar"

---
+++ tabalsMult.js
@@ -0,0 +1,22 @@
+const mult = prompt("ingrese un numero a multiplicar");
+
+for(let i=1;i<=10;i++){
+	
+	let res = document.write(`la multiplicacion de ${mult} x ${i} es ${mult*i} <br>`)
+	
+}
+
+
+/*ESTO ES PARA NODE JS*/
+/*
+var num; 
+process.stdout.write("ingrese un numero a multiplicar");
+ 
+ process.stdin.on('numero', function(numero){
+	 
+	 for(let i=1;i<=10;i++){
+	
+	let res = process.stdout.write(`la multiplicacion de ${numero} x ${i} es ${numero*i} `)
+	 
+};
+*/
